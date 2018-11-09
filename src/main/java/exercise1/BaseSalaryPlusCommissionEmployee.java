e2270befc488119d12f63cfb67278d48f278cda8
samybakk <samy.bakkouche@student.umons.ac.be>
2018-11-09 14:37:00 +0100

    first update

--- src/main/java/exercise1/BaseSalaryPlusCommissionEmployee.java
+++ src/main/java/exercise1/BaseSalaryPlusCommissionEmployee.java
@@ -4,7 +4,9 @@ public class BaseSalaryPlusCommissionEmployee extends Employee{
 
 	public  BaseSalaryPlusCommissionEmployee(String name, int baseSalary, int comission)
 	{
-
+this.name = name;
+this.baseSalary = baseSalary;
+this.comission = comission;
 	}
 
 }
--- src/main/java/exercise1/WorkHourSalaryEmployee.java
+++ src/main/java/exercise1/WorkHourSalaryEmployee.java
@@ -4,6 +4,7 @@ public class WorkHourSalaryEmployee extends Employee {
 
 	public  WorkHourSalaryEmployee(String name, int hourRate)
 	{
-
+this.name = name;
+this.hourRate = hourRate;
 	}
 }
